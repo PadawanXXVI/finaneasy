034858886fb969737df61eb9a368d663e2ada2e1
Anderson de Matos Guimarães <anderson.m.guimaraes@icloud.com>
2026-06-19 11:30:26 -0300

    chore: criar estrutura inicial de diretórios

---
+++ core/session.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def init_session():
+    if "authenticated" not in st.session_state:
+        st.session_state.authenticated = False
+    if "username" not in st.session_state:
+        st.session_state.username = None
+    if "user_id" not in st.session_state:
+        st.session_state.user_id = None
+    if "user_role" not in st.session_state:
+        st.session_state.user_role = None
